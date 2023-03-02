15982219c8e8409c900ae4b74fba62a2ad293705
Brandon Loera Silva <44634476+Yisusmomoa@users.noreply.github.com>
2023-03-02 00:09:42 -0600

    se añade modal, sin contenido en la pantalla de products para poder ordenar y filtrar los productos

--- src/components/Products/SubNavbarProducts.jsx
+++ src/components/Products/SubNavbarProducts.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import { Products_SubNavBar } from './Products.style'
-
-const SubNavbarProducts = () => {
+import ListOutlinedIcon from '@mui/icons-material/ListOutlined';
+const SubNavbarProducts = ({showModal}) => {
   return (
-    <Products_SubNavBar>SubNavbarProducts</Products_SubNavBar>
+    <Products_SubNavBar>
+      <button onClick={showModal}>
+        <ListOutlinedIcon fontSize='large'/>
+      </button>
+    </Products_SubNavBar>
   )
 }
 
--- src/routes/Products.jsx
+++ src/routes/Products.jsx
@@ -5,15 +5,25 @@ import { ProductsContainer} from '../components/Products/Products.style'
 import ListProducts from '../components/Products/ListProducts';
 import AsideProducts from '../components/Products/AsideProducts/AsideProducts';
 import SubNavbarProducts from '../components/Products/SubNavbarProducts';
+import { useModal } from '../state/hooks/useModal';
+import Modal from '../components/IsAdmin.jsx/Modal/Modal';
 
 const Products = () => {
+  const [
+    isOpenOptions,
+    openModalOptions,
+    closeModalOptions
+  ]=useModal()
   return (
     <ThemeProvider theme={theme}>
-        {/* <SubNavbarProducts/> */}
+      <SubNavbarProducts showModal={openModalOptions}/>
       <ProductsContainer>
         <AsideProducts/>
         <ListProducts/>
       </ProductsContainer>
+      <Modal isOpen={isOpenOptions} closeModal={closeModalOptions}>
+        <h1>Options</h1>
+      </Modal>
     </ThemeProvider>
   )
 }
