4453218c13adc7f44db18514416b391e8e912683
Brandon Loera Silva <44634476+Yisusmomoa@users.noreply.github.com>
2023-05-10 01:19:15 -0600

    state/store/servce/BrandService.jsx se modifican los headers del endpoint createBrand

--- src/state/store/service/BrandService.jsx
+++ src/state/store/service/BrandService.jsx
@@ -15,7 +15,19 @@ export const brands=createApi({
                 method:'POST',
                 body,
                 credentials:"include",
-                headers: {'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json'},
+                headers: [
+                    {   'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json'},
+                    {   "key": "Access-Control-Allow-Credentials", "value": "true" },
+                    {   "key": "Access-Control-Allow-Origin", "value": "*" },
+                    {
+                        "key": "Access-Control-Allow-Methods",
+                        "value": "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+                    },
+                    {
+                        "key": "Access-Control-Allow-Headers",
+                        "value": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+                    }
+                ],
             }),
             invalidatesTags:["Brands"]
         }),
