9e228ee2ce39e3f26d1e33c253f6d2c97db74682
Brandon Loera Silva <44634476+Yisusmomoa@users.noreply.github.com>
2023-05-12 01:35:26 -0600

    Se añade servicio para wishlist, se añade funcionalidad para añadir productos a la wishlist del usuario

--- src/components/ProductBody/ProductBodyInfo.jsx
+++ src/components/ProductBody/ProductBodyInfo.jsx
@@ -38,6 +38,8 @@ const ProductBodyInfo = ({productInfo}) => {
       })
     }
   }
+  const [addToWishlist, {isLoading, isSuccess, 
+    isError, error}]=useAddProductToWishListMutation()
   const favoriteIconClick=()=>{
     addToWishlist({id:productInfo?.id})
   }
