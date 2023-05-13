dc6f8ca8f3c30c9caaf27926f36c522de9abec61
Brandon Loera Silva <44634476+Yisusmomoa@users.noreply.github.com>
2023-05-12 18:51:13 -0600

    se agregan tagtypes

--- src/state/store/service/WishlistService.jsx
+++ src/state/store/service/WishlistService.jsx
@@ -3,6 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const wishList=createApi({
     reducerPath:'wishlist',
     baseQuery:fetchBaseQuery({baseUrl:'http://localhost:8080/api/wishList'}),
+    tagTypes:["wishlist"],
     endpoints:(builder)=>({
         addProductToWishList:builder.mutation({
             query:(body)=>({
@@ -10,20 +11,23 @@ export const wishList=createApi({
                 method:'POST',
                 body,
                 credentials:"include",
-            })
+            }),
+            invalidatesTags:["wishlist"]
         }),
         showProductsWishList:builder.query({
             query:()=>({
                 url:'/',
-                credentials:'include'
-            })
+                credentials:'include',
+            }),
+            providesTags:["wishlist"]
         }),
         removeProductWishList:builder.mutation({
             query:(id)=>({
                 url:`/${id}`,
                 method:'DELETE',
                 credentials:"include",
-            })
+            }),
+            invalidatesTags:["wishlist"]
         })
     })
 })
